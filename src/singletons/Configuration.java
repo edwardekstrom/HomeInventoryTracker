9b83eace5459e889021cc76cfb36f6be7659c5a9
Edward Ekstrom <edward.ekstrom@gmail.com>
2013-09-27 10:51:12 -0600

    added configuration singleton.

---
+++ src/singletons/Configuration.java
@@ -0,0 +1,25 @@
+package singletons;
+
+public class Configuration {
+	private static Configuration _instance = null;
+	
+	
+	/**
+	 * This method instantiates the instance of Configuration.
+	 * It will only ever be called one time.
+	 */
+	private Configuration(){
+
+	}
+	
+	/**
+	 * This method returns the Configuration singleton
+	 * @return Configuration, the only instance of it
+	 */
+	public static Configuration getInstance(){
+		if (_instance == null){
+			_instance = new Configuration();
+		}
+		return _instance;
+	}
+}
