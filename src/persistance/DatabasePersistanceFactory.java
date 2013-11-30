3303305b08048aeb4c30dd50e0bc62a8fd8dc33a
capchu <capchu@gmail.com>
2013-11-30 11:43:15 -0700

    Javadocs

--- src/persistance/DatabasePersistanceFactory.java
+++ src/persistance/DatabasePersistanceFactory.java
@@ -8,5 +8,16 @@ package persistance;
  *
  */
 public class DatabasePersistanceFactory extends AbstractPerstistanceFactory {
+	
+	/**Builds a DB Persistance Object
+	 * 
+	 * @precondition no persistor exists
+	 * @postcondition given a DB persistance
+	 * 
+	 * @return returns a db persistance
+	 */
+	public Persistor buildPersistor(){
+		return null;
+	}
 
 }
