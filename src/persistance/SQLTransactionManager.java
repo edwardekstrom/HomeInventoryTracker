1d36bb2e55671c5b1e8672256672fe90b1ddc470
Jaysen Draney <draneyj@gmail.com>
2013-12-04 11:59:35 -0700

    Completed SQLTransactionManager i think….

--- src/persistance/SQLTransactionManager.java
+++ src/persistance/SQLTransactionManager.java
@@ -1,6 +1,7 @@
 package persistance;
 
 import java.sql.*;
+import java.io.File;
 
 
 public class SQLTransactionManager{
@@ -9,16 +10,13 @@ public class SQLTransactionManager{
 
 	public static void initialize() {
 		
-		
 		try {
 			final String driver = "org.apache.derby.jdbc.EmbeddedDriver";
 			Class.forName(driver);
-			
 		}
 		catch(ClassNotFoundException e) {
-			
 		}
-
+		
 	}
 
 	public static Connection getConnection(){
@@ -27,9 +25,43 @@ public class SQLTransactionManager{
 
 	public static void begin(){
 
+		String dbName = "db" + File.separator + "bookclub";
+		String connectionURL = "jdbc:derby:" + dbName + ";create=false";
+		
+		try {
+			assert (_connection == null);
+			
+			_connection = DriverManager.getConnection(connectionURL);
+			_connection.setAutoCommit(false);
+			
+		}
+		catch (SQLException e) {
+		}
+		
 	}
 
-	public static void end(){
+	public static void end(boolean commit){
+		assert (_connection != null);		
 		
+		try {
+			if (commit) {
+				_connection.commit();
+			}
+			else {
+				_connection.rollback();
+			}
+			
+		}
+		catch (SQLException e) {
+		}
+		finally {
+			try{
+				_connection.close();
+			}catch(SQLException e){
+				//Ignore
+			}
+		}
+		
+		_connection = null;
 	}
 }
